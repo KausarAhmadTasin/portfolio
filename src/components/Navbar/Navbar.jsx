4cc86fa6a794499be6c9850d10478a8516c978ec
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2024-02-11 11:03:06 +0600

    navbar started

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Education</li>
+        <li>Contact Information</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
