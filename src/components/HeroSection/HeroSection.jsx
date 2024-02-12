0d61d146d846396ae94fab7b23d9eb1b3492f292
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2024-02-12 20:57:19 +0600

    hero section done

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,9 +1,30 @@
+import "./HeroSection.css";
+import { FaArrowRight } from "react-icons/fa6";
+
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      <p>This is hero section</p>
-      <div className="hero-content"></div>
-      <div className="hero-img-container"></div>
+      <div className="hero-content">
+        <h1 className="hero-greetings">
+          Hello, <span className="name-span">I'm Kausar,</span>
+        </h1>
+        <h2 className="hero-job">Front-End Web Developer</h2>
+        <p className="hero-description">
+          I am a web developer with proficiency in React JS, JavaScript, HTML,
+          and CSS. I thrive on turning creative ideas into innovative and
+          user-friendly web applications.
+        </p>
+        <button className="resume-btn">
+          Resume <FaArrowRight />
+        </button>
+      </div>
+      <div className="hero-img-container  ">
+        <img
+          className="hero-img bubble-border"
+          src="../../../public/h-img.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Kausar Ahmad Tasin</div>
+      <div className="logo">
+        Kausar<span className="last-name">Ahmad Tasin</span>
+      </div>
       <ul className="section-list">
         <button className="primary-btn">Hire Me</button>
         <li>About</li>
-        <li>Projects</li>
         <li>Skills</li>
+        <li>Projects</li>
         <li>Education</li>
         <li>Contact</li>
       </ul>
